2eb89c90e3e1df8e5c9d7cd6a21927f4ffd5dab8
Abhishek Patil <abhishekbpatil16@gmail.com>
2022-09-01 12:16:05 +0530

    Revert "Revert "Sorting rectified""
    This reverts commit 8b14ec63ec75a244f81893670ad1b954b4a0b152.

---
+++ src/stack/udemy/LLImpl/Employee.java
@@ -0,0 +1,54 @@
+package stack.udemy.LLImpl;
+
+import java.util.Objects;
+
+public class Employee {
+	
+	private String name;
+	private String employeeId;
+	
+	public Employee(String name, String employeeId) {
+		super();
+		this.name = name;
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", employeeId=" + employeeId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(employeeId, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(employeeId, other.employeeId) && Objects.equals(name, other.name);
+	}
+
+}
