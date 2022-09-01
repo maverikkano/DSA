8b14ec63ec75a244f81893670ad1b954b4a0b152
Abhishek Patil <abhishekbpatil16@gmail.com>
2022-09-01 12:15:16 +0530

    Revert "Sorting rectified"
    This reverts commit 56bce201574b2ad01cd30d77c37ac2a2b1ec4d72.

--- src/arrays/gfg/level1/Problem_4_sorting_array.java
+++ src/arrays/gfg/level1/Problem_4_sorting_array.java
@@ -1,30 +1,31 @@
 package arrays.gfg.level1;
 
+import java.util.ArrayList;
+
 public class Problem_4_sorting_array {
 
 	public static void main(String[] args) {
-
-		int a[] = { 1, 3, 55, 32, 69, 5 }, n = 6;
-
-		sort(a, n);
-
-		for (int i = 0; i < n; i++)
-			System.out.print(a[i] + " ");
-
-	}
-
-	private static void sort(int[] arr, int n) {
-		int temp = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 1; j < (n - i); j++) {
-				if (arr[j - 1] > arr[j]) {
-					// swap elements
-					temp = arr[j - 1];
-					arr[j - 1] = arr[j];
-					arr[j] = temp;
-				}
-
-			}
+		
+		int a[] = {1,3,55,32,69,5}, n = 6;
+		
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i: a) {
+			arr.add(null)
 		}
+		
+		sort(a, n);
+		
+
 	}
-}
+
+	private int[] sort(int[] a, int n) {
+		
+		if(n <= 1)
+			return a;
+		}
+	
+		for(int i=0; i<n-1; i++)
+			for(int j=0; )
+
+		return a;
+	}
