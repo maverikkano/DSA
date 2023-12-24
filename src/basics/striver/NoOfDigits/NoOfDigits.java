89e4163bf5492225a090c65d8711df7817db1142
Abhishek Patil <abhishekbpatil16@gmail.com>
2023-12-24 14:18:39 +0530

    Testing setup

---
+++ src/basics/striver/NoOfDigits/NoOfDigits.java
@@ -0,0 +1,33 @@
+package basics.striver.NoOfDigits;
+
+import java.util.Scanner;
+
+public class NoOfDigits {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter n: ");
+        int n = in.nextInt();
+
+        int count = getNoOfDigits(n);
+        
+        System.out.println("Count:"+count);
+
+        in.close();
+    }
+
+    public static int getNoOfDigits(int n) {
+
+        int count = 1;
+
+        n = Math.abs(n);
+
+        while (n / 10 >= 1) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+    
+}
---
+++ src/basics/striver/NoOfDigits/NoOfDigitsTest.java
@@ -0,0 +1,75 @@
+package basics.striver.NoOfDigits;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NoOfDigitsTest {
+
+    @Test
+    public void testNegative1Digit() {
+        int input = -5;
+
+        int expected = 1;
+
+        int result = NoOfDigits.getNoOfDigits(input);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void testNegative3Digit() {
+        int input = -235;
+
+        int expected = 3;
+
+        int result = NoOfDigits.getNoOfDigits(input);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void test0() {
+        int input = 0;
+
+        int expected = 1;
+
+        int result = NoOfDigits.getNoOfDigits(input);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void test1Digit() {
+        int input = 5;
+
+        int expected = 1;
+
+        int result = NoOfDigits.getNoOfDigits(input);
+
+        assertEquals(expected,result);
+    }
+
+
+    @Test
+    public void test2Digit() {
+        int input = 55;
+
+        int expected = 2;
+
+        int result = NoOfDigits.getNoOfDigits(input);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void test3Digit() {
+        int input = 235;
+
+        int expected = 3;
+
+        int result = NoOfDigits.getNoOfDigits(input);
+
+        assertEquals(expected,result);
+    }
+
+}
